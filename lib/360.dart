d67436a625d71c6700659531c8faebbe9671c090
sabrinehoiya <sabrine.hoiya@takiacademyteam.com>
2023-01-31 17:52:30 +0100

    add 360 package

---
+++ lib/360.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:imageview360/imageview360.dart';
+
+class Demo360 extends StatefulWidget {
+  Demo360({ key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _DemoPageState createState() => _DemoPageState();
+}
+
+class _DemoPageState extends State<Demo360> {
+  List<ImageProvider> imageList = <ImageProvider>[];
+  bool autoRotate = true;
+  int rotationCount = 2;
+  int swipeSensitivity = 2;
+  bool allowSwipeToRotate = true;
+  RotationDirection rotationDirection = RotationDirection.anticlockwise;
+  Duration frameChangeDuration = const Duration(milliseconds: 50);
+  bool imagePrecached = false;
+
+  @override
+  void initState() {
+    //* To load images from assets after first frame build up.
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => updateImageList(context));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 72.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                (imagePrecached == true)
+                    ? ImageView360(
+                  key: UniqueKey(),
+                  imageList: imageList,
+                  autoRotate: autoRotate,
+                  rotationCount: rotationCount,
+                  rotationDirection: RotationDirection.anticlockwise,
+                  frameChangeDuration: Duration(milliseconds: 30),
+                  swipeSensitivity: swipeSensitivity,
+                  allowSwipeToRotate: allowSwipeToRotate,
+                  onImageIndexChanged: (currentImageIndex) {
+                    print("currentImageIndex: $currentImageIndex");
+                  },
+                )
+                    : Text("Pre-Caching images..."),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Optional features:",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                        fontSize: 24),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text("Auto rotate: $autoRotate"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text("Rotation count: $rotationCount"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text("Rotation direction: $rotationDirection"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text(
+                      "Frame change duration: ${frameChangeDuration.inMilliseconds} milliseconds"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child:
+                  Text("Allow swipe to rotate image: $allowSwipeToRotate"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text("Swipe sensitivity: $swipeSensitivity"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void updateImageList(BuildContext context) async {
+    for (int i = 1; i <= 52; i++) {
+      imageList.add(AssetImage('assets/360/$i.png'));
+      //* To precache images so that when required they are loaded faster.
+      await precacheImage(AssetImage('assets/360/$i.png'), context);
+    }
+    setState(() {
+      imagePrecached = true;
+    });
+  }
+}
